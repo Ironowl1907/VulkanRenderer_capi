8331010dc4b718a22eeb6c7cf5e28765cec36514
Ironowl1907 <facuguialtana7@gmail.com>
2025-12-02 17:32:12 +0000

    Optimizing duplicated vertex

--- Core/src/Renderer/Meshes/Mesh.cpp
+++ Core/src/Renderer/Meshes/Mesh.cpp
@@ -14,18 +14,19 @@ void Mesh::loadFromFile(const char *path) {
   tinyobj::attrib_t attrib;
   std::vector<tinyobj::shape_t> shapes;
   std::vector<tinyobj::material_t> materials;
-  std::string warn;
   std::string err;
+  std::string warn;
 
   if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, path)) {
     throw std::runtime_error(err);
   }
 
+  std::unordered_map<Vertex, uint32_t> uniqueVertices{};
+
   for (const auto &shape : shapes) {
     for (const auto &index : shape.mesh.indices) {
       Vertex vertex{};
 
-      // Load position
       vertex.pos = {attrib.vertices[3 * index.vertex_index + 0],
                     attrib.vertices[3 * index.vertex_index + 1],
                     attrib.vertices[3 * index.vertex_index + 2]};
@@ -34,15 +35,18 @@ void Mesh::loadFromFile(const char *path) {
       if (index.texcoord_index >= 0) {
         vertex.texCoord = {attrib.texcoords[2 * index.texcoord_index + 0],
                            attrib.texcoords[2 * index.texcoord_index + 1]};
-        std::cout << "tex coords" << '\n';
       } else {
-        vertex.texCoord = {0.5f, 0.5f}; // Default texture coordinates
+        vertex.texCoord = {0.0f, 0.0f}; // Default texture coordinates
       }
 
       vertex.color = {1.0f, 1.0f, 1.0f};
 
-      m_vertices.push_back(vertex);
-      m_indices.push_back(m_indices.size());
+      if (uniqueVertices.count(vertex) == 0) {
+        uniqueVertices[vertex] = static_cast<uint32_t>(m_vertices.size());
+        m_vertices.push_back(vertex);
+      }
+
+      m_indices.push_back(uniqueVertices[vertex]);
     }
   }
 }
