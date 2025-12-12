efe8498622a4c1be4b17c164e0115ce099413bd3
Ironowl1907 <facuguialtana7@gmail.com>
2025-12-12 18:26:45 +0000

    Working on descriptors abstraction

---
+++ Core/src/Renderer/Descriptors/Descriptors.cpp
@@ -0,0 +1,49 @@
+#include "Descriptors.h"
+
+#include "vulkan/vulkan_core.h"
+#include <cstdint>
+#include <memory>
+DescriptorSetLayoutBuilder &DescriptorSetLayoutBuilder::addBinding(
+    uint32_t binding, VkDescriptorType descriptorType,
+    VkShaderStageFlags stageFlags, uint32_t count) {
+  assert(m_bindings.count(binding) == 0 && "Binding already in use");
+  VkDescriptorSetLayoutBinding layoutBinding{};
+  layoutBinding.binding = binding;
+  layoutBinding.descriptorType = descriptorType;
+  layoutBinding.descriptorCount = count;
+  layoutBinding.stageFlags = stageFlags;
+  m_bindings[binding] = layoutBinding;
+  return *this;
+}
+
+std::unique_ptr<DescriptorSetLayout> DescriptorSetLayoutBuilder::build() const {
+  return std::make_unique<DescriptorSetLayout>(mp_context, m_bindings);
+}
+
+DescriptorSetLayout::DescriptorSetLayout(
+    VulkanContext *p_context,
+    std::unordered_map<uint32_t, VkDescriptorSetLayoutBinding> bindings)
+    : mp_context(p_context), m_bindings{bindings} {
+  std::vector<VkDescriptorSetLayoutBinding> setLayoutBindings{};
+  for (auto kv : bindings) {
+    setLayoutBindings.push_back(kv.second);
+  }
+
+  VkDescriptorSetLayoutCreateInfo descriptorSetLayoutInfo{};
+  descriptorSetLayoutInfo.sType =
+      VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
+  descriptorSetLayoutInfo.bindingCount =
+      static_cast<uint32_t>(setLayoutBindings.size());
+  descriptorSetLayoutInfo.pBindings = setLayoutBindings.data();
+
+  if (vkCreateDescriptorSetLayout(mp_context->getDevice(),
+                                  &descriptorSetLayoutInfo, nullptr,
+                                  &m_descriptorSetLayout) != VK_SUCCESS) {
+    throw std::runtime_error("failed to create descriptor set layout!");
+  }
+}
+
+DescriptorSetLayout::~DescriptorSetLayout() {
+  vkDestroyDescriptorSetLayout(mp_context->getDevice(), m_descriptorSetLayout,
+                               nullptr);
+}
---
+++ Core/src/Renderer/Descriptors/Descriptors.h
@@ -0,0 +1,98 @@
+#pragma once
+
+#include "Swapchain/Swapchain.h"
+#include <memory>
+class DescriptorSetLayout {
+public:
+  DescriptorSetLayout(
+      VulkanContext *p_context,
+      std::unordered_map<uint32_t, VkDescriptorSetLayoutBinding> bindings);
+  ~DescriptorSetLayout();
+  DescriptorSetLayout(const DescriptorSetLayout &) = delete;
+  DescriptorSetLayout &operator=(const DescriptorSetLayout &) = delete;
+
+  VkDescriptorSetLayout getDescriptorSetLayout() const {
+    return m_descriptorSetLayout;
+  }
+
+private:
+  VulkanContext *mp_context;
+  VkDescriptorSetLayout m_descriptorSetLayout;
+  std::unordered_map<uint32_t, VkDescriptorSetLayoutBinding> m_bindings;
+
+  friend class DescriptorWriter;
+};
+
+class DescriptorSetLayoutBuilder {
+public:
+  DescriptorSetLayoutBuilder(VulkanContext *p_context)
+      : mp_context(p_context) {};
+
+  DescriptorSetLayoutBuilder &addBinding(uint32_t binding,
+                                         VkDescriptorType descriptorType,
+                                         VkShaderStageFlags stageFlags,
+                                         uint32_t count = 1);
+  std::unique_ptr<DescriptorSetLayout> build() const;
+
+private:
+  VulkanContext *mp_context;
+  std::unordered_map<uint32_t, VkDescriptorSetLayoutBinding> m_bindings{};
+};
+
+class DescriptorPool {
+public:
+  DescriptorPool(VulkanContext *p_context, uint32_t maxSets,
+                 VkDescriptorPoolCreateFlags poolFlags,
+                 const std::vector<VkDescriptorPoolSize> &poolSizes);
+  ~DescriptorPool();
+  DescriptorPool(const DescriptorPool &) = delete;
+  DescriptorPool &operator=(const DescriptorPool &) = delete;
+
+  bool allocateDescriptor(const VkDescriptorSetLayout descriptorSetLayout,
+                          VkDescriptorSet &descriptor) const;
+
+  void freeDescriptors(std::vector<VkDescriptorSet> &descriptors) const;
+
+  void resetPool();
+
+private:
+  VulkanContext *mp_context;
+  VkDescriptorPool descriptorPool;
+
+  friend class DescriptorWriter;
+};
+
+class DescriptorPoolBuilder {
+public:
+  DescriptorPoolBuilder(VulkanContext *p_context) : mp_context(p_context) {}
+
+  DescriptorPoolBuilder &addPoolSize(VkDescriptorType descriptorType,
+                                     uint32_t count);
+  DescriptorPoolBuilder &setPoolFlags(VkDescriptorPoolCreateFlags flags);
+  DescriptorPoolBuilder &setMaxSets(uint32_t count);
+  DescriptorPool *build() const;
+
+private:
+  VulkanContext *mp_context;
+  std::vector<VkDescriptorPoolSize> poolSizes{};
+  uint32_t maxSets = 1000;
+  VkDescriptorPoolCreateFlags poolFlags = 0;
+};
+
+class DescriptorWriter {
+public:
+  DescriptorWriter(DescriptorSetLayout &setLayout, DescriptorPool &pool);
+
+  DescriptorWriter &writeBuffer(uint32_t binding,
+                                VkDescriptorBufferInfo *bufferInfo);
+  DescriptorWriter &writeImage(uint32_t binding,
+                               VkDescriptorImageInfo *imageInfo);
+
+  bool build(VkDescriptorSet &set);
+  void overwrite(VkDescriptorSet &set);
+
+private:
+  DescriptorSetLayout &setLayout;
+  DescriptorPool &pool;
+  std::vector<VkWriteDescriptorSet> writes;
+};
